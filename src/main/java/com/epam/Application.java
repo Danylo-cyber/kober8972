0bef2f3fede7941698b983fb47b93adbbd87eb61
Danylo <kober8972@gmail.com>
2019-09-20 13:57:20 +0300

    bonachchi

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -3,42 +3,52 @@ package com.epam;
 import java.util.Scanner;
 
 public class Application {
-       public static void main(String[] args) {
-Task5();
-       }
-    public static void Task5(){
-
-        Scanner inp1 = new Scanner(System.in);
-        System.out.println("Enter number x");
-        int x = inp1.nextInt();
-        Scanner inp2 = new Scanner(System.in);
-        System.out.println("Enter number y");
-        int y = inp2.nextInt();
-        for(int k = 0; k < y; k++){
-            for(int i = 0; i < x; i++ ){
-                System.out.print(" * ");
-            }
-            System.out.println();
+    public static void main(String[] args) {
+        int n0 = 1;
+        int n1 = 1;
+        int n2;
+         System.out.print(n0 + " "+n1 + " ");
+        for (int i = 3; i <= 11; i++) {
+            n2 = n0 + n1;
+            System.out.print(n2 + " ");
+            n0 = n1;
+            n1 = n2;
         }
-
-
+        System.out.println();
     }
-            //System.out.println("darova daun");
-           // System.out.println("давай вводь число, а я його суммую з усіма числами до нього:");
-           // Scanner input = new Scanner(System.in);
-          //  int value= input.nextInt();
-           // int sum = 0;
-           // for (int i=1;i<=value;i++){
-             //   sum = sum + i;}
-           // System.out.println("Мій магічний шар показує: " + sum);
-        //}
-        //System.out.println("enter a number");
-        //for (int i = 3; i <= 10; i++) {
+    // public static void Task5(){
 
-       // Scanner input = new Scanner(System.in);
-       // int value = input.nextInt();
-       // System.out.println(value); };
+    //Scanner inp1 = new Scanner(System.in);
+    //System.out.println("Enter number x");
+    //int x = inp1.nextInt();
+    //Scanner inp2 = new Scanner(System.in);
+    //System.out.println("Enter number y");
+    //int y = inp2.nextInt();
+    //for(int k = 0; k < y; k++){
+    //  for(int i = 0; i < x; i++ ){
+    //    System.out.print(" * ");
+    //}
+    //System.out.println();
+    //}
+
+
 }
+//System.out.println("darova daun");
+// System.out.println("давай вводь число, а я його суммую з усіма числами до нього:");
+// Scanner input = new Scanner(System.in);
+//  int value= input.nextInt();
+// int sum = 0;
+// for (int i=1;i<=value;i++){
+//   sum = sum + i;}
+// System.out.println("Мій магічний шар показує: " + sum);
+//}
+//System.out.println("enter a number");
+//for (int i = 3; i <= 10; i++) {
+
+// Scanner input = new Scanner(System.in);
+// int value = input.nextInt();
+// System.out.println(value); };
+//}
 
 
 
