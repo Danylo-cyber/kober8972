5a956f001f925fc737d4c0a71e42665e407f4044
Danylo <kober8972@gmail.com>
2019-09-20 12:32:22 +0300

    table

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -3,6 +3,9 @@ package com.epam;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello");
+        for(int i=3; i<=10 ; i++) {
+            System.out.println("3 * " + i + " " + "= " + i*3);
+            }
     }
 }
 
